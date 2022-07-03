bdb4fb63d093831340da78ce1d1400ce05c52cfa
irania90 <wwwwwq37@gmail.com>
2022-07-03 20:06:58 +0430

    add '+=' addition to Calculator class.

--- files/calculator.py
+++ files/calculator.py
@@ -1,16 +1,21 @@
 class Calculator:
     def __init__(self, value = 0):
-        #set value to sort in memory
+        #Set value to sort in memory
         self.value = value
 
     def __repr__(self):
-        # called when you print Calculator object
+        #To get alled when you print Calculator object
         return repr(self.value)
     
     def checker(self, value):
-        # check if value is float or integer
+        #Check if value is float or integer
         if isinstance(value, (int, float)):
             return True
         else:
             raise ValueError(f"{value} is not integer or float.")
         
+    def __iadd__(self, value):
+        #To get called on addition with assignment e.g. a +=b.
+        if self.checker(value):
+            self.value += value
+            return Calculator(self.value)
