f67644974c2d7bf8ab6351ece99efc50463efefb
irania90 <wwwwwq37@gmail.com>
2022-07-03 20:07:54 +0430

    add '-=' subtraction to Calculator class.

--- files/calculator.py
+++ files/calculator.py
@@ -1,14 +1,14 @@
 class Calculator:
     def __init__(self, value = 0):
-        #Set value to sort in memory
+        #Set value to sort in memory.
         self.value = value
 
     def __repr__(self):
-        #To get alled when you print Calculator object
+        #To get alled when you print Calculator object.
         return repr(self.value)
     
     def checker(self, value):
-        #Check if value is float or integer
+        #Check if value is float or integer.
         if isinstance(value, (int, float)):
             return True
         else:
@@ -19,3 +19,11 @@ class Calculator:
         if self.checker(value):
             self.value += value
             return Calculator(self.value)
+            
+    def __isub__(self, value):
+        #To get called on subtraction with assignment e.g. a -=b.
+        if self.checker(value):
+            self.value -= value
+            return Calculator(self.value)
+
+            
