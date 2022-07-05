a71f7875fc4084c9ff9d9303ffeffb2b9682f87f
irania90 <wwwwwq37@gmail.com>
2022-07-05 14:21:02 +0430

    add number handler and insert them to echo box and change gont size automatically

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit
 from PyQt5 import QtGui
 import sys
+from functools import partial
 
 
 class Window(QMainWindow):
@@ -17,9 +18,32 @@ class Window(QMainWindow):
         # set the title
         self.setWindowTitle("Calculator")
 
+        #Insert numer to #echo
+        for number in range(0,10):
+            self.findChild(QPushButton, f"b_{number}").clicked.connect( partial(self.insert_number, number) )
+        
         # show all the widgets
         self.show()
 
+
+    def insert_number(self, number):
+        try:
+            echo = self.findChild(QLineEdit, "echo")
+            numbers = echo.text()
+            echo.setText( numbers + str(number) )
+            count = len( numbers + str(number))
+
+            if count > 18 :
+                new_font_size = echo.fontInfo().pointSize() - 1 
+                if new_font_size > 8 :
+                    new_font = echo.font()
+                    new_font.setPointSize(new_font_size)
+                    echo.setFont(new_font)
+
+ 
+    
+        except Exception as e:
+            print(e)
 
 # create pyqt5 app
 App = QApplication(sys.argv)
