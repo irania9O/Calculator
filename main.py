d723542df0f90f958b475aa37c3a4990e2f7d9b3
irania90 <wwwwwq37@gmail.com>
2022-07-07 21:03:04 +0430

    add the exp function and perform your operation.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt5.QtGui import QKeySequence, QIcon
 import sys
 from functools import partial
 from files.calculator import Calculator
-from math import factorial, pi, sqrt, pow, sin, cos, tan, log10
+from math import exp, factorial, pi, sqrt, pow, sin, cos, tan, log10
 class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
@@ -120,12 +120,37 @@ class Window(QMainWindow):
         b_log = self.findChild(QPushButton, "b_log")
         b_log.clicked.connect( self.log ) 
 
+        # exp
+        b_log_2 = self.findChild(QPushButton, "b_log_2")
+        b_log_2.clicked.connect( self.exp ) 
+
         #operator button
         self.b_operator = self.findChild(QPushButton, "b_operator")
 
         # show all the widgets
         self.show()
 
+    def exp(self):
+        echo =  self.findChild(QLineEdit, "echo")
+        value = self.findChild(QLineEdit, "value")
+        data = echo.text()
+        last_data = value.text()
+        try:
+            if data == "":
+                if last_data != "":
+                    self.calculator.exp() 
+                    value.setText(str(self.calculator.value))
+            else:
+                new_info =  exp(float(data))
+                echo.setText(str(new_info))
+        except :
+            msg = QMessageBox()
+            msg.setWindowIcon(QIcon("images/calculator_icon.png"))
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("An error occurred.")
+            msg.setWindowTitle("Error")
+            msg.exec_()
+            
     def log(self):
         echo =  self.findChild(QLineEdit, "echo")
         value = self.findChild(QLineEdit, "value")
